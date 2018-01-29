a9205979a03dc9a258adef4652c7901afb0e735a
Franklin Waller <fr.l.waller@gmail.com>
2018-01-29 16:37:37 +0100

    feat(wepack): Update webpack tasks to v1.1.2 to support localIdentName

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const packageConfig = require('./package.json');
 module.exports = {
     output: {
         path: path.resolve(__dirname, './build'),
-        filename: packageConfig.main + '.js',
+        filename: 'meister.bundled.js',
         publicPath: '/build/',
         libraryTarget: 'umd',
     },
