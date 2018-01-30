ff614099988d98b44c1238146705293844f7fc8d
Franklin Waller <fr.l.waller@gmail.com>
2018-01-30 13:29:42 +0100

    feat(plugins): Update plugins to latest version

--- src/js/MeisterPlayer.js
+++ src/js/MeisterPlayer.js
@@ -6,7 +6,7 @@ import HtmlUi from '@meisterplayer/plugin-htmlui/index';
 import NativeHls from '@meisterplayer/plugin-nativehls';
 import Hls from '@meisterplayer/plugin-hls';
 import MultiSource from '@meisterplayer/plugin-multisource';
-import GoogleAnalytics from '@npm-wearetriple/meister-plugin-googleanalytics';
+// import GoogleAnalytics from '@npm-wearetriple/meister-plugin-googleanalytics';
 
 // import Dash from '@meisterplayer/plugin-dash';
 // import DebugOverlay from '@meisterplayer/plugin-debugoverlay/index';
@@ -15,8 +15,8 @@ import GoogleAnalytics from '@npm-wearetriple/meister-plugin-googleanalytics';
 // import RtlPlayer from '@npm-wearetriple/meister-plugin-rtlplayer/index';
 // import StreamSense from '@npm-wearetriple/meister-plugin-streamsense/index';
 // import Drm from '@npm-wearetriple/meister-plugin-drm/index';
-// import GoogleIma from '@npm-wearetriple/meister-plugin-googleima/index';
-// import AdItem from '@npm-wearetriple/meister-plugin-aditem/index';
+import GoogleIma from '@npm-wearetriple/meister-plugin-googleima/index';
+import AdItem from '@npm-wearetriple/meister-plugin-aditem/index';
 // import Conviva from '@npm-wearetriple/meister-plugin-conviva/index';
 // import ChromecastSender from '@npm-wearetriple/meister-plugin-chromecastsender/index';
 // import AdobeAnalytics from '@npm-wearetriple/meister-plugin-adobeanalytics/index';
@@ -34,13 +34,13 @@ Meister.builtIn = {
     [NativeHls.pluginName]: {},
     [Hls.pluginName]: {},
     [MultiSource.pluginName]: {},
-    [GoogleAnalytics.pluginName]: {},
+    // [GoogleAnalytics.pluginName]: {},
     // [Dash.pluginName]: {},
     // [StreamSense.pluginName]: {},
     // [WebVtt.pluginName]: {},
     // [Drm.pluginName]: {},
-    // [GoogleIma.pluginName]: {},
-    // [AdItem.pluginName]: {},
+    [GoogleIma.pluginName]: {},
+    [AdItem.pluginName]: {},
     // [Conviva.pluginName]: {},
     // [Smooth.pluginName]: {},
     // [DebugOverlay.pluginName]: {},
